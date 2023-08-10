bfb2a1fb3069724c743139ffa4b11f3b628fe8ae
Allan Bolipata <bolipatc@mskcc.org>
2023-08-10 16:19:13 -0400

    Add config and env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+# placeholder script to load necessary dependencies, environment
+
+export TMPDIR=/scratch/voyager
+export NXF_SINGULARITY_CACHEDIR=/juno/work/ci/dev/dev_phoenix/singularity_cachedir
+
+# load necessary required packages, specific to JUNO
+# edit to your needs
+module load java/jdk-11.0.11
+module load singularity/3.7.1
